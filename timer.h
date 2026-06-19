a863ac2008c5f1e9bab119c8a87bcc1798cbc1a9
dimcrap <mamad.rahmani235@gmail.com>
2026-06-19 20:49:40 +0330

    implmenting timer class and its methods

--- timer.h
+++ timer.h
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <functional>
+#include <vector>
 
 
 
@@ -7,11 +8,35 @@ class timer {
     struct  scheduledTask
     {
         std::chrono::steady_clock::time_point next_excution;
-        std::chrono::seconds;
-
+        std::chrono::seconds interval;
+        std::function<void()> callback;
+        int id;
     };
-    
+
+    std::vector<scheduledTask> tasks;
+    int nextId{0};
 
     public:
-    private:
+
+    void schedule(std::function <void()> task,std::chrono::seconds interval){
+        tasks.push_back({
+            std::chrono::steady_clock::now()+ interval,
+            interval,
+            task,
+            nextId++
+        });
+    };
+
+    void tick(){
+        auto now=std::chrono::steady_clock::now();
+
+        for (auto & task: tasks){
+            if(now>=task.next_excution){
+                task.callback();
+                task.next_excution=now+task.interval;
+            }
+        }
+    };
+    
+    
 }
